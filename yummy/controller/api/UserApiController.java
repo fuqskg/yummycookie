3763f37c291b995c33f4c8599bfeecc2f4365fbb
fuqskg <106498001+fuqskg@users.noreply.github.com>
2023-04-21 02:15:46 +0900

    로그인_ㅁㅣ완

--- yummy/controller/api/UserApiController.java
+++ yummy/controller/api/UserApiController.java
@@ -4,11 +4,10 @@ import com.cookie.yummy.dto.ResponseDto;
 import com.cookie.yummy.entity.MemberEntity;
 import com.cookie.yummy.entity.RoleType;
 import com.cookie.yummy.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserApiController {
@@ -25,5 +24,27 @@ public class UserApiController {
         memberEntity.setRole(RoleType.USER);
         int result = memberService.join(memberEntity);
         return new ResponseDto<Integer>(HttpStatus.OK.value(), result); // 자바 오브젝트를 JSON으로 변환하여 전송(Jackson)
+    }
+
+    //전통적방식 로그인
+    @PostMapping("/yummy/login")
+    public ResponseDto<Integer> login(@RequestBody MemberEntity memberEntity, HttpSession session){
+        System.out.println("UAC: login 호출됨");
+
+        MemberEntity principal = memberService.login(memberEntity); //principal(접근주체)
+        if(principal != null){
+            session.setAttribute("principal", principal); //principal을 키값으로 하고 그대로 담으면 세션이 만들어짐
+        }
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); // 자바 오브젝트를 JSON으로 변환하여 전송(Jackson)
+
+
+    }
+
+    // 이메일 중복검사
+    @PostMapping("/yummy/email-check")
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail) {
+        System.out.println("memberEmail = " + memberEmail);
+        String checkResult = memberService.emailCheck(memberEmail);
+        return checkResult;
     }
 }
